8db68d1b03c213b7ad84fe08bcf62fa05a8eccc4
Ivan <gorgeouswintered@gmail.com>
2023-11-13 01:46:59 +0300

    Changed dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://nedomarket:8003',
+  server: {
+    proxy: {
+      '/api': 'http://nedomarket:8003',
     },
+    host: true,
+    port: 3000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true
+    }
   },
   plugins: [react()],
 })
